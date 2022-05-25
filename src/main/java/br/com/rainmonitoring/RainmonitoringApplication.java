21bf734298cafb591b2cedf85fdb14ced009ef9f
2020nani <nanialmeida2019@gmail.com>
2022-05-25 03:46:20 -0300

    revisando

--- src/main/java/br/com/rainmonitoring/RainmonitoringApplication.java
+++ src/main/java/br/com/rainmonitoring/RainmonitoringApplication.java
@@ -1,14 +1,17 @@
 package br.com.rainmonitoring;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableScheduling
+@CrossOrigin("${host.access.cors}")
 public class RainmonitoringApplication {
 
 	public static void main(String[] args) {
